edd370ba78376831dde788d662ebcf574fe1949e
lioisshi <84262564+lioisshi@users.noreply.github.com>
2021-05-29 13:29:32 +0900

    Initial Commit

---
+++ SwiftUI-Section5/SwiftUI_Section5App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Section5App.swift
+//  SwiftUI-Section5
+//
+//  Created by Hiroaki Ishiga on 2021/05/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Section5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
